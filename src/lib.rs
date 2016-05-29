3859febf6d70879204cc04e2ebc82a7be731cb7e
Kartikaya Gupta <kats@seldon.staktrace.com>
2016-05-28 23:45:03 -0400

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,9 @@ pub enum QuotedPrintableError {
 }
 
 pub fn decode(input: &str, mode: ParseMode) -> Result<Vec<u8>, QuotedPrintableError> {
-    let filtered = input.chars().filter(|&c| c == '\t' || c == '\r' || c == '\n' || (c >= ' ' && c <= '~')).collect::<String>();
+    let filtered = input.chars()
+        .filter(|&c| c == '\t' || c == '\r' || c == '\n' || (c >= ' ' && c <= '~'))
+        .collect::<String>();
     if mode == ParseMode::Strict && filtered.len() != input.len() {
         return Err(QuotedPrintableError::InvalidByte);
     }
@@ -68,11 +70,13 @@ pub fn decode(input: &str, mode: ParseMode) -> Result<Vec<u8>, QuotedPrintableEr
                 let lower_char = lower as char;
                 if upper_char.is_digit(16) && lower_char.is_digit(16) {
                     if mode == ParseMode::Strict {
-                        if upper_char.to_uppercase().next() != Some(upper_char) || lower_char.to_uppercase().next() != Some(lower_char) {
+                        if upper_char.to_uppercase().next() != Some(upper_char) ||
+                           lower_char.to_uppercase().next() != Some(lower_char) {
                             return Err(QuotedPrintableError::LowercaseHexOctet);
                         }
                     }
-                    let combined = upper_char.to_digit(16).unwrap() << 4 | lower_char.to_digit(16).unwrap();
+                    let combined = upper_char.to_digit(16).unwrap() << 4 |
+                                   lower_char.to_digit(16).unwrap();
                     decoded.push(combined as u8);
                 } else {
                     if mode == ParseMode::Strict {
@@ -96,28 +100,56 @@ mod tests {
 
     #[test]
     fn test_decode() {
-        assert_eq!("hello world", String::from_utf8(decode("hello world", ParseMode::Strict).unwrap()).unwrap());
+        assert_eq!("hello world",
+                   String::from_utf8(decode("hello world", ParseMode::Strict).unwrap()).unwrap());
         assert_eq!("Now's the time for all folk to come to the aid of their country.",
-                   String::from_utf8(decode("Now's the time =\r\nfor all folk to come=\r\n to the aid of their country.", ParseMode::Strict).unwrap()).unwrap());
-        assert_eq!("\r\nhello=world", String::from_utf8(decode("=0D=0Ahello=3Dworld", ParseMode::Strict).unwrap()).unwrap());
-        assert_eq!("hello world\r\ngoodbye world", String::from_utf8(decode("hello world\r\ngoodbye world", ParseMode::Strict).unwrap()).unwrap());
-        assert_eq!("hello world\r\ngoodbye world", String::from_utf8(decode("hello world   \r\ngoodbye world   ", ParseMode::Strict).unwrap()).unwrap());
-        assert_eq!("hello world\r\ngoodbye world ", String::from_utf8(decode("hello world   \r\ngoodbye world =  ", ParseMode::Strict).unwrap()).unwrap());
+                   String::from_utf8(decode("Now's the time =\r\nfor all folk to come=\r\n to \
+                                             the aid of their country.",
+                                            ParseMode::Strict)
+                           .unwrap())
+                       .unwrap());
+        assert_eq!("\r\nhello=world",
+                   String::from_utf8(decode("=0D=0Ahello=3Dworld", ParseMode::Strict).unwrap())
+                       .unwrap());
+        assert_eq!("hello world\r\ngoodbye world",
+                   String::from_utf8(decode("hello world\r\ngoodbye world", ParseMode::Strict)
+                           .unwrap())
+                       .unwrap());
+        assert_eq!("hello world\r\ngoodbye world",
+                   String::from_utf8(decode("hello world   \r\ngoodbye world   ",
+                                            ParseMode::Strict)
+                           .unwrap())
+                       .unwrap());
+        assert_eq!("hello world\r\ngoodbye world ",
+                   String::from_utf8(decode("hello world   \r\ngoodbye world =  ",
+                                            ParseMode::Strict)
+                           .unwrap())
+                       .unwrap());
 
         assert_eq!(true, decode("hello world=x", ParseMode::Strict).is_err());
-        assert_eq!("hello world=x", String::from_utf8(decode("hello world=x", ParseMode::Robust).unwrap()).unwrap());
+        assert_eq!("hello world=x",
+                   String::from_utf8(decode("hello world=x", ParseMode::Robust).unwrap()).unwrap());
 
         assert_eq!(true, decode("hello world=3d", ParseMode::Strict).is_err());
-        assert_eq!("hello world=", String::from_utf8(decode("hello world=3d", ParseMode::Robust).unwrap()).unwrap());
+        assert_eq!("hello world=",
+                   String::from_utf8(decode("hello world=3d", ParseMode::Robust).unwrap())
+                       .unwrap());
 
         assert_eq!(true, decode("hello world=3m", ParseMode::Strict).is_err());
-        assert_eq!("hello world=3m", String::from_utf8(decode("hello world=3m", ParseMode::Robust).unwrap()).unwrap());
+        assert_eq!("hello world=3m",
+                   String::from_utf8(decode("hello world=3m", ParseMode::Robust).unwrap())
+                       .unwrap());
 
         assert_eq!(true, decode("hello\u{FF}world", ParseMode::Strict).is_err());
-        assert_eq!("helloworld", String::from_utf8(decode("hello\u{FF}world", ParseMode::Robust).unwrap()).unwrap());
+        assert_eq!("helloworld",
+                   String::from_utf8(decode("hello\u{FF}world", ParseMode::Robust).unwrap())
+                       .unwrap());
 
-        assert_eq!(true, decode("12345678901234567890123456789012345678901234567890123456789012345678901234567", ParseMode::Strict).is_err());
-        assert_eq!("12345678901234567890123456789012345678901234567890123456789012345678901234567", String::from_utf8(decode("12345678901234567890123456789012345678901234567890123456789012345678901234567", ParseMode::Robust).unwrap()).unwrap());
-        assert_eq!("1234567890123456789012345678901234567890123456789012345678901234567890123456", String::from_utf8(decode("1234567890123456789012345678901234567890123456789012345678901234567890123456", ParseMode::Strict).unwrap()).unwrap());
+        assert_eq!(true,
+                   decode("12345678901234567890123456789012345678901234567890123456789012345678901234567", ParseMode::Strict).is_err());
+        assert_eq!("12345678901234567890123456789012345678901234567890123456789012345678901234567",
+                   String::from_utf8(decode("12345678901234567890123456789012345678901234567890123456789012345678901234567", ParseMode::Robust).unwrap()).unwrap());
+        assert_eq!("1234567890123456789012345678901234567890123456789012345678901234567890123456",
+                   String::from_utf8(decode("1234567890123456789012345678901234567890123456789012345678901234567890123456", ParseMode::Strict).unwrap()).unwrap());
     }
 }
